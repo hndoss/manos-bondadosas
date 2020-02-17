06e6efaf2587986333b9b06a6f5403be6a782055
Hector Rosales <hector.rosales@acklenavenue.com>
2020-02-17 08:17:18 -0600

    created github pages.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
+import VS2 from 'vue-script2';
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   vuetify,
+  VS2,
   render: h => h(App)
 }).$mount('#app')
