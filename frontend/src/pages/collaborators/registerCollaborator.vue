4e61e8ff1f32541234f2d4ddb24445d25d048311
Hector Rosales <hector.rosales@acklenavenue.com>
2020-05-04 17:52:31 -0600

    added collaborator add component.

--- frontend/src/pages/collaborators/registerCollaborator.vue
+++ frontend/src/pages/collaborators/registerCollaborator.vue
@@ -1,70 +1,101 @@
 <template>
-  <v-row align="center">
-    <v-row justify="space-around">
-      <v-switch v-model="valid" class="ma-4" label="Valid" readonly></v-switch>
-      <v-switch v-model="lazy" class="ma-4" label="Lazy"></v-switch>
-    </v-row>
-    <v-form ref="form" v-model="valid" :lazy-validation="lazy">
-      <v-text-field v-model="name" :counter="10" :rules="nameRules" label="Name" required></v-text-field>
+  <div>
+    <form>
+      <v-col xs9 sm8 md8 lg8>
+        <v-container>
+          <v-text-field v-model="firstName" :counter="10" label="First Name" required></v-text-field>
+          <v-text-field v-model="lastName" :counter="10" label="Last Name" required></v-text-field>
+          <v-text-field v-model="age" :counter="10" label="Age" required></v-text-field>
+          <v-text-field v-model="sex" :counter="10" label="Sex" required></v-text-field>
+          <v-text-field v-model="email" :counter="10" label="Email" required></v-text-field>
 
-      <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+          <!-- <v-text-field
+            v-model="firstName"
+            :counter="10"
+            label="Last Name"
+            required
+            @input="$v.name.$touch()"
+            @blur="$v.name.$touch()"
+          ></v-text-field>Age - Combobox
+          <v-radio-group :mandatory="true">
+            <v-radio label="Male" value="M"></v-radio>
+            <v-radio label="Feminine" value="F"></v-radio>
+          </v-radio-group>
 
-      <v-select
-        v-model="select"
-        :items="items"
-        :rules="[v => !!v || 'Item is required']"
-        label="Item"
-        required
-      ></v-select>
+          <v-text-field
+            v-model="email"
+            label="E-mail"
+            required
+            @input="$v.email.$touch()"
+            @blur="$v.email.$touch()"
+          ></v-text-field>
 
-      <v-checkbox
-        v-model="checkbox"
-        :rules="[v => !!v || 'You must agree to continue!']"
-        label="Do you agree?"
-        required
-      ></v-checkbox>
+          <v-text-field v-model="name" :counter="10" label="Age" required></v-text-field>
+          <v-text-field v-model="name" :counter="10" label="Sex" required></v-text-field>-->
+        </v-container>
+      </v-col>
 
-      <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate">Validate</v-btn>
+      <v-col xs3 sm4 md4 lg4>
+        <v-container>
+          <v-checkbox label="Do you agree?" required></v-checkbox>
+        </v-container>
+      </v-col>
 
-      <v-btn color="error" class="mr-4" @click="reset">Reset Form</v-btn>
-
-      <v-btn color="warning" @click="resetValidation">Reset Validation</v-btn>
-    </v-form>
-  </v-row>
+      <v-btn class="mr-4" @click="submit">Submit</v-btn>
+      <!-- <v-btn @click="clear">clear</v-btn> -->
+    </form>
+  </div>
 </template>
 
 <script>
-export default {
+import { validationMixin } from "vuelidate";
+import { required, maxLength, email } from "vuelidate/lib/validators";
+import axios from "axios";
+
+export default {  
+  mixins: [validationMixin],
   name: "registerCollaborator",
+  validations: {
+    name: { required, maxLength: maxLength(10) },
+    email: { required, email },
+    select: { required },
+    checkbox: {
+      checked(val) {
+        return val;
+      }
+    }
+  },
   data: () => ({
-    valid: true,
-    name: "",
-    nameRules: [
-      v => !!v || "Name is required",
-      v => (v && v.length <= 10) || "Name must be less than 10 characters"
-    ],
+    firstName: "",
+    lastName: "",
     email: "",
-    emailRules: [
-      v => !!v || "E-mail is required",
-      v => /.+@.+\..+/.test(v) || "E-mail must be valid"
-    ],
-    select: null,
-    items: ["Item 1", "Item 2", "Item 3", "Item 4"],
-    checkbox: false,
-    lazy: false
+    age: "",
+    sex: ""
   }),
 
   methods: {
-    validate() {
-      if (this.$refs.form.validate()) {
-        this.snackbar = true;
-      }
+    clear() {
+      this.$v.$reset();
+      this.name = "";
+      this.email = "";
+      this.select = null;
+      this.checkbox = false;
     },
-    reset() {
-      this.$refs.form.reset();
-    },
-    resetValidation() {
-      this.$refs.form.resetValidation();
+    submit() {
+      this.$v.$touch();
+      console.log('send');
+      let post = {
+        first_name: this.firstName,
+        last_name: this.lastName,
+        email: this.email,
+        age: this.age,
+        sex: this.sex
+      };
+      axios
+        .post("http://localhost:8000/api/v1/collaborators/", post)
+        .then(result => {
+          console.log(result);
+        });
     }
   }
 };
